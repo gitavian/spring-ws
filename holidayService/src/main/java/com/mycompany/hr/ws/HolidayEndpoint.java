9f29ad048236538e916bb89a63c2379309b49381
Octavian Mac <balaban.octavian@yahoo.com>
2014-11-09 23:40:35 +0200

    Added new sample project. Managed to generate automatically java classes from xsd.

--- holidayService/src/main/java/com/mycompany/hr/ws/HolidayEndpoint.java
+++ holidayService/src/main/java/com/mycompany/hr/ws/HolidayEndpoint.java
@@ -1,14 +1,19 @@
 package com.mycompany.hr.ws;
 
+import com.mycompany.hr.schemas.HolidayRequest;
+import com.mycompany.hr.schemas.HolidayResponse;
 import com.mycompany.hr.service.HumanResourceService;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.Namespace;
-import org.jdom.xpath.XPath;
+
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.xpath.XPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,6 +22,7 @@ import java.util.Date;
 /**
  * Created by obalaban on 11/5/2014.
  */
+
 @Endpoint
 public class HolidayEndpoint {
 
@@ -46,14 +52,19 @@ public class HolidayEndpoint {
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "HolidayRequest")
-    public void handleHolidayRequest(@RequestPayload Element holidayRequest)
+    public @ResponsePayload HolidayResponse holidayResponse (@RequestPayload HolidayRequest holidayRequest)
             throws Exception {
 
+        HolidayResponse response = new HolidayResponse();
+        System.out.println("-----> Entered in the payload");
+
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date startDate = dateFormat.parse(startDateExpression.valueOf(holidayRequest));
-        Date endDate = dateFormat.parse(endDateExpression.valueOf(holidayRequest));
-        String name = nameExpression.valueOf(holidayRequest);
+        Date startDate = new Date();
+        Date endDate = new Date();
+        String name = "Name";
 
         humanResourceService.bookHoliday(startDate, endDate, name);
+
+        return response;
     }
 }
